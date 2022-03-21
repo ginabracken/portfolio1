c1b0ff29a67f652379ab8caabd0f33474c618fba
gibracken <93899099+gibracken@users.noreply.github.com>
2022-03-21 16:38:46 -0400

    commit

--- lab5/src/App.js
+++ lab5/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Menu from "./components/Menu";
+import React from "react";
 
 function App() {
+  let menuData = [
+    {
+      menuId: "menu1",
+      menuName: "Go Do Something!",
+      menuItems: [
+        {
+          itemId: 1,
+          itemDescription: "Pick an excercise:",
+        },
+      ],
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {menuData.map((menu) => {
+        return <Menu key={menu.menuId} data={menu} />;
+      })}
     </div>
   );
 }
